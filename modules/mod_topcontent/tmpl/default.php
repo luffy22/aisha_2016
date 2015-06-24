addec87a63c52791dd192cf71bf946f2d7434a8f
luffy22 <kopnite@gmail.com>
2015-06-24 06:22:16 +0530

    mod_topcontent bootstrap panel added jquery ui panel removed

--- modules/mod_topcontent/tmpl/default.php
+++ modules/mod_topcontent/tmpl/default.php
@@ -6,40 +6,54 @@ defined('_JEXEC') or die;
 ?>
 <div class="spacer"></div>
 <div>
-<div role="tabpanel"><!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-<li class="active" role="presentation"><a href="#topcontent-1" role="tab" data-toggle="tab">Most Read</a></li>
-<li role="presentation"><a href="#topcontent-2" role="tab" data-toggle="tab">Recent Popular</a></li>
+<li class="active" role="presentation"><a href="#tp-1" role="tab" data-toggle="tab">Most Read</a></li>
+<li role="presentation"><a href="#tp-2" role="tab" data-toggle="tab">Recent Popular</a></li>
 </ul>
-</div>
 <!-- Tab panes -->
 <div class="tab-content">
-    <div class="spacer"></div>
-<div id="topcontent-1" class="tab-pane active" role="tabpanel">
+<div class="spacer"></div>
+<div id="tp-1" class="tab-pane active" role="tabpanel">
+   <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
 <?php
+$i=1;
 foreach($topview as $data)
 {
     $data->slug = $data->article_id.':'.$data->article_alias;
     $data->catslug = $data->catid.':'.$data->cat_alias;
     $data->link = JRoute::_(ContentHelperRoute::getArticleRoute($data->slug, $data->catslug));
 ?>
-<h3><?php echo trim($data->title); ?></h3> 
-<div>
+<div class="panel panel-primary">
+    <div class="panel-heading" role="tab" id="heading_<?php echo $i; ?>">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapse_<?php echo $i; ?>" 
+           aria-expanded="true" aria-controls="collapse_<?php echo $i; ?>">
+          <?php echo trim($data->title); ?>
+        </a>
+      </h4>
+    </div>
+    <div id="collapse_<?php echo $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_<?php echo $i; ?>">
+      <div class="panel-body">
     <div><strong>Hits: <?php echo $data->hits; ?></strong></div>
-<p>
+    <p>
+    <?php
+
+            echo substr(strip_tags(trim($data->introtext)), 1, 1000);
+            echo "....";
+    ?>
+        <a href="<?php echo $data->link; ?>" title="<?php echo $data->title; ?>">Read More</a>
+    </p>    
+      </div>
+    </div>
+  </div>
 <?php
-        
-        echo substr(strip_tags(trim($data->introtext)), 1, 1000);
-        echo "....";
-?>
-    <a href="<?php echo $data->link; ?>" title="<?php echo $data->title; ?>">Read More</a>
-</p>    
-</div>
-<?php
+$i++;
 }
 ?>
 </div>
-<div id="topcontent-2" class="tab-pane" role="tabpanel">
+</div>
+<div id="tp-2" class="tab-pane" role="tabpanel">
+   <div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
 <?php
 foreach($toprecent as $data)
 {
@@ -47,22 +61,36 @@ foreach($toprecent as $data)
     $data->catslug = $data->catid.':'.$data->cat_alias;
     $data->link = JRoute::_(ContentHelperRoute::getArticleRoute($data->slug, $data->catslug));
 ?>
-<h3><?php echo trim($data->title); ?></h3> 
-<div>
+<div class="panel panel-primary">
+    <div class="panel-heading" role="tab" id="heading_<?php echo $i; ?>">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapse_<?php echo $i; ?>" 
+           aria-expanded="true" aria-controls="collapse_<?php echo $i; ?>">
+          <?php echo trim($data->title); ?>
+        </a>
+      </h4>
+    </div>
+    <div id="collapse_<?php echo $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading_<?php echo $i; ?>">
+      <div class="panel-body">
     <div><strong>Hits: <?php echo $data->hits; ?></strong></div>
-<p>
+    <p>
+    <?php
+
+            echo substr(strip_tags(trim($data->introtext)), 1, 1000);
+            echo "....";
+    ?>
+        <a href="<?php echo $data->link; ?>" title="<?php echo $data->title; ?>">Read More</a>
+    </p>    
+      </div>
+    </div>
+  </div>
 <?php
-        
-        echo substr(strip_tags(trim($data->introtext)), 1, 1000);
-        echo "....";
-?>
-    <a href="<?php echo $data->link; ?>" title="<?php echo $data->title; ?>">Read More</a>
-</p>    
-</div>
-<?php
+$i++;
 }
 ?>
 </div>
 </div>
+<!-- End of tabs -->
+</div> <!-- class="tab-content" -->
 </div>
 <div class="spacer"></div>
