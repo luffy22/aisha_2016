ab6c6dc6d688ff9c3414822a66e5e38b1191409f
luffy22 <kopnite@gmail.com>
2016-03-10 15:39:07 +0530

    mod_topmenu done 2016-03-10

--- modules/mod_topmenu/helper.php
+++ modules/mod_topmenu/helper.php
@@ -16,10 +16,68 @@ class modTopMenuHelper
         $menu = $app->getMenu();
 
         // Get active menu item
-        $base = self::getBase($params);
-        $items   = $menu->getItems('menutype', $base->menutype);
+        $base       = self::getBase($params);
+        $result     = $menu->getItems('menutype', $base->menutype);
+?>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="<?php echo JUri::base(); ?>" class="navbar-brand">Astro Isha</a>
+    </div>
+<div class="navbar-collapse collapse" id="navbar">
+  <ul class="nav navbar-nav">
+<?php
+        foreach($result as $items)
+        {
+            
+            if($items->level !== '2')
+            {
+                continue;
+            }
+       ?>
+            <li class="dropdown navbar-inverse">
+          <?php
+            if($items->level=="2")
+            {
+                $children       = $menu->getItems('parent_id',$items->id, false);
+          ?>
+             <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown" class="dropdown-toggle" href="<?php echo $items->link; ?>"><?php echo $items->title ?><span class="caret"></span></a>
+             <ul class="navbar navbar-nav navbar-inverse dropdown-menu">
+             <?php
+                    foreach($children as $child)
+                    {
+                    ?>
+                         <li><a href="<?php echo $child->link; ?>" title="<?php echo $child->title; ?>"><?php echo $child->title; ?></a></li>
+                 <?php
+                    }
+             ?>
+             </ul>
+      <?php
+            }
+      ?>
+            </li>
+<?php
+      }
+?>
+            </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+<?php
+    }
+    public function getChildren($title, $link, $id)
+    {
+        $app        = JFactory::getApplication();
+        $menu       = $app->getMenu()->getItems('parent_id',$id, true);
+        $result     = array("title"=>$title, "link"=>$link,"id"=>$id,$menu);
+        return $result;      
         
-        return $items;
     }
     public static function getBase(&$params)
     {
@@ -41,6 +99,11 @@ class modTopMenuHelper
 
         return $base;
     }
-    
+    public static function getActive(&$params)
+    {
+        $menu = JFactory::getApplication()->getMenu();
+
+        return $menu->getActive() ? $menu->getActive() : $menu->getDefault();
+    }
         
 }
--- templates/astroisha2.0/index.php
+++ templates/astroisha2.0/index.php
@@ -3,10 +3,10 @@
     defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" 
-   xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
-<head>
+<html lang="<?php echo $this->lang; ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/style.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/bootstrap/css/bootstrap.min.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/bootstrap/css/bootstrap-theme.min.css" type="text/css" />
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery.min.js" type="text/javascript" language="javascript"></script>
@@ -14,8 +14,17 @@
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/common.js" type="text/javascript" language="javascript"></script>
 </head>
 <body>
-<jdoc:include type="modules" name="topmenu" /> 
-<jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
+<jdoc:include type="modules" name="topmenu" />
+<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner"> <div class="container"> <div class="navbar-header"> <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a href="../" class="navbar-brand">Bootstrap</a> </div> <nav id="bs-navbar" class="collapse navbar-collapse"> <ul class="nav navbar-nav"> <li> <a href="../getting-started/">Getting started</a> </li> <li class="active"> <a href="../css/">CSS</a> </li> <li> <a href="../components/">Components</a> </li> <li> <a href="../javascript/">JavaScript</a> </li> <li> <a href="../customize/">Customize</a> </li> </ul> <ul class="nav navbar-nav navbar-right"> <li><a href="http://themes.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Themes&quot;)">Themes</a></li> <li><a href="http://expo.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Expo&quot;)">Expo</a></li> <li><a href="http://blog.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Blog&quot;)">Blog</a></li> </ul> </nav> </div> </header>
+<div class="bs-docs-header" id="content" tabindex="-1"> <div class="container"> <h1>CSS</h1> <p>Global CSS settings, fundamental HTML elements styled and enhanced with extensible classes, and an advanced grid system.</p> <div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script>var z=document.createElement("script");z.async=!0,z.src="http://engine.carbonads.com/z/32341/azcarbon_2_1_0_HORIZ";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(z,s);</script></div></div></div></div>
+<div class="fluid-container">
+<div class="col-xs-12 col-md-8">
+    <jdoc:include type="component" />
+    <jdoc:include type="modules" name="bottom" />
+</div>
+<div class="col-xs-6 col-md-4">
+    <jdoc:include type="modules" name="sidebar" />
+</div> 
+</div>
 </body>
 </html>
