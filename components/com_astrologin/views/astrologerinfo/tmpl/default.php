60dc8bbf11f24da9f6d4c155c0e0bd33be71ca05
luffy22 <kopnite@gmail.com>
2016-10-27 15:21:13 +0530

    Astrologer Registration Main Page in-progress 2016-10-27

---
+++ components/com_astrologin/views/astrologerinfo/tmpl/default.php
@@ -0,0 +1,77 @@
+<script type="text/javascript">
+$(function() {
+    $( "#astro-reg" ).accordion({
+      heightStyle: "content",
+      collapsible: true
+    });
+  });
+</script>
+<?php
+defined('_JEXEC') or die('Restricted access');
+?>
+<div class="spacer"></div>
+<div id="astro-reg">
+<h3>Welcome To Astro Isha</h3>
+<div>
+	
+<p>&nbsp;&nbsp;&nbsp;<img src="<?php echo JURI::base() ?>images/jv_logo3.png" title="Welcome To Astro Isha" class="img-responsive" align="left" />If you happen to be an Astrologer, Palmist, Feng-Shui, Vastu Shastra Expert, 
+involved in karmakand, poojas or rituals then kindly go through the contents of this page.</p>
+</div>
+<h3>Registering With Astro Isha</h3>
+<div>
+<p>&nbsp;&nbsp;&nbsp;Astro Isha offers a chance to Register and post your details just like 
+with Online Directories and Yellow Pages. But it is much more then an Online Identity Card for an Astrologer. 
+It is a place where you can post your articles, get comments, feedback related to your work and most important of all 
+receive Online Payments subject to being a Paid Member.</p>
+</div>
+<h3>Account Types</h3>
+<div>
+<p>There are two types of accounts which can be opened with Astro Isha. They are: </p>
+<ol>
+<li><strong>Free Account:</strong> Free Account lets you register and share your details with Users. Once you are registered and 
+authenticated your email, you would be provided an option on 
+your dashboard to open Free Account. Once it is opened you can change your details as and when you please.</li>
+<li><strong>Paid Account:</strong> Paid Account lets you register, share your details, post articles on the 
+website, receive online payments and also reply to client queries online. Once you have registered and authenticated your 
+email there would be an option on your Dashboard to avail Paid Services. Click that option and also choose one of the payment types 
+to complete Payment in order to avail Paid Services.</li>
+</ol>
+</div>
+<h3>Paid Account- How To Pay?</h3>
+<div>
+<p>&nbsp;&nbsp;&nbsp;Astrologers who wish to avail services like receiving online payments, posting articles, 
+posting client reports online need to pay a certain amount to Astro Isha. This is a one-time payment only 
+and no further fees or charges would be imposed on Astrologer for keeping Account on this website. This one-time 
+payment is to encourage genuine Astrologers who wish to make money from their expertise. Those who wish to open a 
+Paid Account can pay via: </p>
+<ol>
+<li><strong>Online Payment:</strong> Details of Online Payment would be provided in an Email. Valid in All Countries apart from Pakistan and few other countries</li>
+<li><strong>Direct Transfer:</strong> Transfer Details would be provided in Email. Valid in All Countries(Kindly check with your bank if Money Transfer is possible)</li>
+<li><strong>Cheque:</strong> Details for Cheque would be provided in Email. Valid in India Only</li>
+</ol>
+</div>
+<h3>Receiving Payments- How It Works?</h3>
+<div>
+<img src="<?php echo JURI::base() ?>images/payment_process.png" title="Welcome To Astro Isha" class="img-responsive" />
+<p>&nbsp&nbsp;&nbsp;Astrologer who have opened <strong>Paid Account</strong> can receive Online Payments from customers. If a customer 
+    decides to use your expertise then he can do so via <a href="<?php echo JURI::base() ?>ask-astrologer">Ask An Astrologer</a> page. Once he 
+ orders your Services you have Fifteen Days(15 Days) to answer to his/her queries. Once the answer has been provided you 
+would receive Online Payment for your Consultation Services after deduction of some amount.</p>
+</div>
+<h3>Charges, Taxes and Conversion Rates</h3>
+<div>
+<p>&nbsp;&nbsp;&nbsp;Astro Isha charges 10% of the total charge meaning if your fees for online consultation is 30 $(US Dollars) then 3$(US Dollars) goes 
+to Astro Isha as Commission over Consultation Fees. There is also Service Charges and Service Tax which are automatically deducted by 
+Third-Party Gateways(CCAvenue For Indian Customers and Paypal For Rest Of World). In addition Paypal also applies 
+Conversion Charges and Astro Isha has no say in those matters. Astrologers are advised to plan Fees for their Online Consultation after 
+considering charges by Astro Isha, related Taxes, Payment Gateway charges and also Conversion Rates.</p>
+</div>
+<h3>For More Information</h3>
+<div>
+<p>&nbsp;&nbsp;&nbsp;If you find the <a href="<?php echo JURI::base() ?>astro-register" title="Register with Astro Isha">Registration</a> Process difficult then kindly get in 
+touch at email: admin@astroisha.com. Alternatively you can call or whatsapp to +91-9727841461 for more details.</p>
+</div>
+</div> <!-- astro-reg -->
+<div class="spacer"></div>
+<div class="text-right"><a href="<?php echo JURI::base() ?>astro-register" class="btn btn-primary">Registration</a></div>
+<div class="spacer"></div>
