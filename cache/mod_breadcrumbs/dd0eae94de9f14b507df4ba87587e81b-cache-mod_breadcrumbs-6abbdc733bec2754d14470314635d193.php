85cc2ed256f4344fe4f4dfd6dcb579920ccc83d1
luffy22 <kopnite@gmail.com>
2016-11-02 15:06:22 +0530

    cache updated

--- cache/mod_breadcrumbs/dd0eae94de9f14b507df4ba87587e81b-cache-mod_breadcrumbs-6abbdc733bec2754d14470314635d193.php
+++
@@ -1,4 +0,0 @@
-<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";a:2:{s:4:"body";s:0:"";s:4:"head";a:0:{}}s:6:"result";s:402:"
-<ul class="breadcrumb">
-	<li class="active">You are here: &#160;</li><li><a href="/" class="pathway">Home</a><span class="divider"><img src="/media/system/images/arrow.png" alt="" /></span></li><li><a href="/12-ascendants" class="pathway">Twelve Ascendants</a><span class="divider"><img src="/media/system/images/arrow.png" alt="" /></span></li><li class="active"><span>Main Articles</span></li></ul>
-";}
--- cache/mod_breadcrumbs/dd0eae94de9f14b507df4ba87587e81b-cache-mod_breadcrumbs-b9b6e4db690d2992228d14d1fc45a9b0.php
+++
@@ -1,6 +0,0 @@
-<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";a:2:{s:4:"body";s:0:"";s:4:"head";a:2:{s:7:"scripts";a:1:{s:30:"/media/jui/js/bootstrap.min.js";a:3:{s:4:"mime";s:15:"text/javascript";s:5:"defer";b:0;s:5:"async";b:0;}}s:6:"script";a:1:{s:15:"text/javascript";s:107:"jQuery(document).ready(function(){
-	jQuery('.hasTooltip').tooltip({"html": true,"container": "body"});
-});";}}}s:6:"result";s:397:"
-<ul class="breadcrumb">
-	<li class="active">You are here: &#160;</li><li><a href="/" class="pathway">Home</a><span class="divider"><img src="/media/system/images/arrow.png" alt="" /></span></li><li><a href="/nakshatras" class="pathway">Nakshatras</a><span class="divider"><img src="/media/system/images/arrow.png" alt="" /></span></li><li class="active"><span>Ashlesha Nakshatra</span></li></ul>
-";}
