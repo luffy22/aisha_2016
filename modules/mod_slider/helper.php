6f16baeccf94413126fdbb238f0d2f55a8c8f28e
luffy22 <kopnite@gmail.com>
2016-12-05 06:56:53 +0530

    mod_slider minor problems 2016-12-05 @home

---
+++ modules/mod_slider/helper.php
@@ -0,0 +1,61 @@
+<?php
+defined('_JEXEC') or die;
+
+require_once JPATH_SITE . '/components/com_content/helpers/route.php';
+
+JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_content/models', 'ContentModel');
+
+class modSliderHelper
+{
+    function getArticles($params)
+    {
+        // Get an instance of the generic articles model
+        $model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+        // Set application parameters in model
+        $app = JFactory::getApplication();
+        $appParams = $app->getParams();
+        $model->setState('params', $appParams);
+
+        // Access filter
+        $access = !JComponentHelper::getParams('com_content')->get('show_noauth');
+        $authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
+        $model->setState('filter.access', $access);
+
+        // Category filter
+        $model->setState('filter.category_id', $params->get('catid', array()));
+
+        // Filter by language
+        $model->setState('filter.language', $app->getLanguageFilter());
+        // Set the filters based on the module params
+        $model->setState('list.start', 0);
+        $model->setState('list.limit', (int) $params->get('count', 12));
+        $model->setState('filter.published', 1);
+        $model->setState('filter.featured', $params->get('show_front', 1) == 1 ? 'show' : 'hide');
+        // Ordering
+        $model->setState('list.ordering', 'a.id');
+        $model->setState('list.direction', 'ASC');
+
+        $items = $model->getItems();
+
+        foreach ($items as &$item)
+        {
+                $item->slug = $item->id . ':' . $item->alias;
+                $item->catslug = $item->catid . ':' . $item->category_alias;
+
+                if ($access || in_array($item->access, $authorised))
+                {
+                        // We know that user has the privilege to view the article
+                        $item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catslug));
+                }
+                else
+                {
+                        $item->link = JRoute::_('index.php?option=com_users&view=login');
+                }
+        }
+
+        return $items;
+     
+    }
+}
+
+?>
---
+++ modules/mod_slider/tmpl/default.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_popular
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+$count = count($list);
+$i = 0;
+?>
+<div id="article_caroussel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+  <?php
+  while($i < $count)
+  {
+      if($i == 0)
+      {
+  ?>
+    <li data-target="#article_caroussel" data-slide-to="<?php echo $i; ?>" class="active"></li>
+  <?php
+      }
+    else
+    {
+  ?>
+    <li data-target="#article_caroussel" data-slide-to="<?php echo $i; ?>"></li>
+<?php
+    }
+    if($i >= $count)$i = 0;else $i++;
+  }    
+?>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+   <?php
+        $j = 0; 
+        foreach($list as $item)
+        {
+        $images = json_decode($item->images);
+        if($j == 0)
+        {
+    ?>
+            <div class="item active">
+                <img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo $item->title; ?>">
+                <div class="carousel-caption">
+                   <h3><a href="<?php echo $item->link ?>"><?php echo $item->title; ?></a></h3>
+                    <p><?php echo $item->metadesc; ?></p>
+              </div>
+            </div>
+   <?php
+        }
+ else {
+   ?>
+      <div class="item">
+                <img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo $item->title; ?>">
+                <div class="carousel-caption">
+                   <h3><a href="<?php echo $item->link ?>"><?php echo $item->title; ?></a></h3>
+                   <p><?php echo $item->metadesc; ?></p>
+              </div>
+            </div>
+  <?php
+  
+  }
+        $j++;
+        }
+ if($j >= $count)$j =0;
+  
+?>
+    
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#article_caroussel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#article_caroussel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
